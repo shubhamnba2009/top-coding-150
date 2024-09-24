c15472742588ad905be197fa80470ffb6088df1c
Shubham Rajput <shubhamnba2009@gmail.com>
2024-09-24 16:43:55 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -2,38 +2,27 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
-        int[][] dp = new int[m][n];
 
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                dp[i][j]=mat[i][j];
-            }
-        }
-
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
-                if(dp[i][j]==0)
-                    continue;
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                if(mat[i][j]==0) continue;
                 int minVal = m*n;
-                if(i>0) minVal = Math.min(minVal, dp[i-1][j]);
-                if(j>0) minVal = Math.min(minVal, dp[i][j-1]);
-
-                dp[i][j] = 1+minVal; 
+                if(i>0) minVal = Math.min(minVal, mat[i-1][j]);
+                if(j>0) minVal = Math.min(minVal, mat[i][j-1]);
+                mat[i][j] = 1+minVal; 
             }
         }
 
-        for(int i = m-1;i>=0;i--){
-            for(int j = n-1;j>=0;j--){
-                if(dp[i][j] == 0)
-                    continue;
-
+        for(int i =m-1;i>=0;i--){
+            for(int j =n-1;j>=n;j--){
+                if(mat[i][j]==0) continue;
                 int minVal = m*n;
-                if(i<m-1) minVal = Math.min(minVal, dp[i+1][j]);
-                if(j<n-1) minVal = Math.min(minVal, dp[i][j+1]);
-
-                dp[i][j] = Math.min(1+minVal, dp[i][j]);
+                if(i<m-1) minVal = Math.min(minVal, mat[i+1][j]);
+                if(j<n-1) minVal = Math.min(minVal, mat[i][j+1]);
+                mat[i][j] = Math.min(mat[i][j],1+minVal); 
             }
         }
-        return dp;
+
+        return mat;
     }
 }
