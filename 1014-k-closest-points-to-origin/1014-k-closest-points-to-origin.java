e625325c5aae06011842ae0e7512657188514d9c
Shubham Rajput <shubhamnba2009@gmail.com>
2024-09-28 20:23:41 +0100

    Time: 25 ms (72.44%), Space: 52.7 MB (55.41%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (a,b)->(a[0]*a[0]+a[1]*a[1])-(b[0]*b[0]+b[1]*b[1]));
+        Arrays.sort(points, (a, b)->(a[0]*a[0]+a[1]*a[1])-(b[0]*b[0]+b[1]*b[1]));
 
         int[][] res = new int[k][2];
 
