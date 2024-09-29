9802ba4815dfdf1fe49e43350dc9301f7f98c87b
Shubham Rajput <shubhamnba2009@gmail.com>
2024-09-29 22:11:47 +0100

    Time: 0 ms (100%), Space: 40.9 MB (79.23%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -17,10 +17,12 @@ class Solution {
     public TreeNode invertTree(TreeNode root) {
         if(root==null)
             return root;
-        TreeNode left = invertTree(root.left);
-        TreeNode right = invertTree(root.right);
-        root.left = right;
-        root.right = left;
-        return root;
+
+        TreeNode tmp = invertTree(root.left);
+        root.left = invertTree(root.right);
+        root.right = tmp;
+
+    return root;
+
     }
 }
