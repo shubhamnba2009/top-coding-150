fe859151319d82470217764cb3b47dca2e7169df
Shubham Rajput <shubhamnba2009@gmail.com>
2024-09-29 22:10:08 +0100

    Time: 0 ms (100%), Space: 44.2 MB (59.74%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -17,14 +17,17 @@ class Solution {
     public boolean isBalanced(TreeNode root) {
         if(root==null)
             return true;
+
         if(Math.abs(height(root.left)-height(root.right))>1)
             return false;
+
         return isBalanced(root.left)&&isBalanced(root.right);
+
         
     }
 
-    public int height(TreeNode root){
-        if(root == null)
+    public int height(TreeNode root) {
+        if(root==null)
             return 0;
         return 1+Math.max(height(root.left), height(root.right));
     }
