5115612c3dcfe73d20936eea44cda506f42cefe0
Shubham Rajput <shubhamnba2009@gmail.com>
2024-09-30 16:45:59 +0100

    Time: 0 ms (100%), Space: 41.2 MB (52.94%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,31 +1,30 @@
 class MyQueue {
-    private static Stack<Integer> fStack;
-    private static Stack<Integer> sStack;
+    Stack<Integer> st1;
+    Stack<Integer> st2;
     public MyQueue() {
-        fStack = new Stack<>();
-        sStack = new Stack<>();
+        st1 = new Stack<>();
+        st2 = new Stack<>();
     }
     
     public void push(int x) {
-        while(!fStack.isEmpty()){
-            sStack.add(fStack.pop());
-        }
-        fStack.add(x);
-        while(!sStack.isEmpty()){
-            fStack.add(sStack.pop());
+        if(st1.isEmpty()) st1.add(x);
+        else{
+            while(!st1.isEmpty()) st2.add(st1.pop());
+            st1.add(x);
+            while(!st2.isEmpty()) st1.add(st2.pop());
         }
     }
     
     public int pop() {
-        return fStack.pop();
+        return st1.pop();
     }
     
     public int peek() {
-        return fStack.peek();
+        return st1.peek();
     }
     
     public boolean empty() {
-        return fStack.isEmpty();
+        return st1.isEmpty();
     }
 }
 
