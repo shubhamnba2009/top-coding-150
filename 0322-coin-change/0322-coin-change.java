ba2324d8efec3f931fa54f70779e6061a6c300c6
Shubham Rajput <shubhamnba2009@gmail.com>
2024-10-01 17:21:43 +0100

    Time: 24 ms (36.15%), Space: 44.4 MB (53.19%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,25 @@
+class Solution {
+    int[] memo;
+    public int coinChange(int[] coins, int amount) {
+        memo = new int[amount+1];
+        return dpHelper(coins, amount);
+    }
+
+    public int dpHelper(int[] coins, int amount){
+        if(amount==0)
+            return 0;
+        if(amount<0)
+            return -1;
+        if(memo[amount] != 0)
+            return memo[amount];
+        int minAmt = Integer.MAX_VALUE;
+        for(int coin:coins){
+            int amt = dpHelper(coins, amount-coin);
+            if(amt==-1) continue;
+            minAmt = Math.min(1+amt, minAmt);
+        }
+
+        memo[amount] = minAmt == Integer.MAX_VALUE?-1:minAmt;
+        return memo[amount];
+    }
+}
