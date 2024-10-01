b420f556b8a629c503d588eff0e27a6c4b8d202a
Shubham Rajput <shubhamnba2009@gmail.com>
2024-10-01 21:38:55 +0100

    Time: 4 ms (82.8%), Space: 45.2 MB (45.08%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -2,39 +2,33 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adjList = new ArrayList<>(numCourses);
 
-        for(int i = 0;i<numCourses;i++){
-            adjList.add(new ArrayList<>());
+        for(int i  = 0;i<numCourses;i++){
+            adjList.add(i, new ArrayList<>());
         }
 
-        for(int[] prerequisite:prerequisites){
-            adjList.get(prerequisite[1]).add(prerequisite[0]);
+        for(int[] pre: prerequisites){
+            adjList.get(pre[1]).add(pre[0]);
         }
 
         boolean[] stack = new boolean[numCourses];
         boolean[] visited = new boolean[numCourses];
 
-        for(int n = 0;n<numCourses;n++){
-            if(dfs(n, adjList, stack, visited)){
+        for(int i = 0;i<numCourses;i++){
+            if(dfs(adjList, stack, visited, i))
                 return false;
-            }
         }
         return true;
     }
 
-    private boolean dfs(int n, List<List<Integer>> adjList, boolean[] stack, boolean[] visited) {
-        if(stack[n])
-            return true;
-        if(visited[n])
-            return false;
+    public boolean dfs(List<List<Integer>> adjList, boolean[] stack, boolean[] visited, int n){
+        if(stack[n])    return true;
+        if(visited[n])  return false;
 
-        visited[n] = true;
-        stack[n] = true;
+        stack[n] = true; visited[n] = true;
 
-        for(int i:adjList.get(n)){
-            if(dfs(i, adjList, stack, visited))
-                return true;
+        for(int node: adjList.get(n)){
+            if(dfs(adjList, stack, visited, node)) return true;
         }
-
         stack[n] = false;
         return false;
     }
