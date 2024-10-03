aa9489e9dd2a7e7d22ab7cbca09bec012d6c2d3d
Shubham Rajput <shubhamnba2009@gmail.com>
2024-10-03 22:38:09 +0100

    Time: 3 ms (16.13%), Space: 42.1 MB (75.7%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -2,44 +2,45 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         Queue<Pair<Integer, Integer>> queue = new ArrayDeque<>();
 
-        int ROWS = grid.length, COLS = grid[0].length;
-        int freshOranges = 0;
+        int ROWS = grid.length, COLS = grid[0].length, freshOranges = 0;
 
         for(int i = 0;i<ROWS;i++){
-            for(int j =0;j<COLS;j++){
-                if(grid[i][j]==2) queue.offer(new Pair(i,j));
-                else if (grid[i][j]==1) freshOranges++;
+            for(int j = 0; j<COLS;j++){
+                if(grid[i][j]==2) queue.offer(new Pair(i, j));
+                else if(grid[i][j]==1) freshOranges++;
             }
         }
+
         queue.offer(new Pair(-1,-1));
-        int[][] direction = {{1,0},{0,1},{-1,0},{0,-1}};
         int time = -1;
+        int[][] directions = {{1,0},{0,1},{-1,0},{0,-1}};
 
         while(!queue.isEmpty()){
             Pair<Integer, Integer> p = queue.poll();
             int row = p.getKey();
             int col = p.getValue();
 
-            if(row==-1){
+            if(row == -1){
                 time++;
                 if(!queue.isEmpty()){
                     queue.offer(new Pair(-1,-1));
                 }
             }
             else{
-                for(int[] dir:direction){
-                    int nRow = row+dir[0];
-                    int nCol = col+dir[1];
-                    if(nRow>=0&&nRow<ROWS&&
-                    nCol>=0&&nCol<COLS){
-                        if(grid[nRow][nCol]==1){
-                            grid[nRow][nCol] = 2;
-                            freshOranges--;
-                            queue.offer(new Pair(nRow, nCol));
-                        }
+            for(int[] dir:directions){
+                int nRow = row+dir[0];
+                int nCol = col+dir[1];
+                if(nRow>=0&&nRow<ROWS&&
+                nCol>=0&&nCol<COLS){
+                    if(grid[nRow][nCol]==1){
+                    grid[nRow][nCol]=2;
+                    freshOranges--;
+                    queue.offer(new Pair(nRow, nCol));
                     }
                 }
             }
+            }
+
         }
         return freshOranges==0?time:-1;
     }
