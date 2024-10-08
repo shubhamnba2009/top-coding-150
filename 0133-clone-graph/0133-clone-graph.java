9c39416da7ca9a49437674f755a9d62364660d35
Shubham Rajput <shubhamnba2009@gmail.com>
2024-10-08 19:02:14 +0100

    Time: 28 ms (20.15%), Space: 42.6 MB (57.27%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -23,14 +23,13 @@ class Solution {
     public Node cloneGraph(Node node) {
         if(node == null)
             return node;
-        
         if(visited.containsKey(node))
             return visited.get(node);
 
         Node cloneNode = new Node(node.val, new ArrayList<>());
         visited.put(node, cloneNode);
 
-        for(Node n : node.neighbors){
+        for(Node n:node.neighbors){
             cloneNode.neighbors.add(cloneGraph(n));
         }
         return cloneNode;
