15ae8bdf62945339de24b2156d171ba3b0e0aec2
Shubham Rajput <shubhamnba2009@gmail.com>
2024-10-09 22:01:14 +0100

    Time: 1 ms (99.8%), Space: 44.6 MB (35.89%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<>();
-        dphelper(0, candidates, target, res, new ArrayList<>());
+        combi(0, candidates, target, res, new ArrayList<>());
         return res;
     }
 
-    public void dphelper(int idx, int[] candidates, int target, List<List<Integer>> res, List<Integer> ds){
+    public void combi(int idx, int[] candidates, int target, List<List<Integer>> res, List<Integer> ds){
         if(idx == candidates.length){
-            if(target == 0){
-                res.add(new ArrayList<>(ds));
-            }
-            return;
+        if(target == 0){
+            res.add(new ArrayList<>(ds));
+        }
+        return;
         }
 
         if(target>=candidates[idx]){
             ds.add(candidates[idx]);
-            dphelper(idx, candidates, target-candidates[idx], res, ds);
+            combi(idx, candidates, target-candidates[idx], res, ds);
             ds.remove(ds.size()-1);
         }
-
-        dphelper(idx+1, candidates, target, res, ds);
+        combi(idx+1, candidates, target, res, ds);
     }
 }
