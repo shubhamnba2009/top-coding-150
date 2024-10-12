1e614137034b19f2647003c1aee3200291f68276
Shubham Rajput <shubhamnba2009@gmail.com>
2024-10-12 18:05:37 +0100

    Time: 6 ms (99.47%), Space: 47.8 MB (23.44%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,23 +1,27 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int m = mat.length, n = mat[0].length;
+        int r = mat.length, c = mat[0].length;
 
-        for(int i = 0;i<m;i++){
-            for(int j = 0;j<n;j++){
+        for(int i = 0;i<r;i++){
+            for(int j = 0;j<c;j++){
+                int minVal = r*c;
                 if(mat[i][j] == 0) continue;
-                int minVal = m*n;
-                if(i>0) minVal = Math.min(minVal, mat[i-1][j]);
-                if(j>0) minVal = Math.min(minVal, mat[i][j-1]);
+
+                if(i>0)    minVal = Math.min(minVal, mat[i-1][j]);
+                if(j>0)    minVal = Math.min(minVal, mat[i][j-1]);
+
                 mat[i][j] = 1+minVal;
             }
         }
 
-        for(int i = m-1;i>=0;i--){
-            for(int j = n-1;j>=0;j--){
+        for(int i = r-1;i>=0;i--){
+            for(int j = c-1;j>=0;j--){
+                int minVal = r*c;
                 if(mat[i][j] == 0) continue;
-                int minVal = m*n;
-                if(i<m-1) minVal = Math.min(minVal, mat[i+1][j]);
-                if(j<n-1) minVal = Math.min(minVal, mat[i][j+1]);
+
+                if(i<r-1)    minVal = Math.min(minVal, mat[i+1][j]);
+                if(j<c-1)    minVal = Math.min(minVal, mat[i][j+1]);
+
                 mat[i][j] = Math.min(mat[i][j], 1+minVal);
             }
         }
