3d929d645dcb56ddafb7363c5a6dbdb79269f0c0
Shubham Rajput <shubhamnba2009@gmail.com>
2024-10-20 06:37:14 +0100

    Time: 9 ms (99.26%), Space: 57.5 MB (18.95%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,21 +1,26 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int totalSum = 0;
-        for(int i :nums)    totalSum+=i;
-        if(totalSum%2!=0)   return false;
-        int subSetSum = totalSum/2;
-        int n = nums.length;
-        Boolean[][] memo = new Boolean[n][subSetSum+1];
-        return dfsHelper(nums, subSetSum, n-1, memo);
-        
-    }
-    private boolean dfsHelper(int[] nums, int subSetSum, int idx, Boolean[][] memo){
-            if(subSetSum==0)    return true;
-            if(idx==0||subSetSum<0) return false;
-            if(memo[idx][subSetSum]!=null) return memo[idx][subSetSum];
+        int totSum = 0;
 
-            boolean result = dfsHelper(nums, subSetSum-nums[idx-1], idx-1, memo) || dfsHelper(nums, subSetSum, idx-1, memo);
-            memo[idx][subSetSum] = result;
-            return result;
+        for(int n : nums) totSum +=n;
+        if(totSum%2!=0) return false;
+
+        int subSetSum = totSum / 2;
+        int n = nums.length;
+        Boolean[][] flag = new Boolean[n][subSetSum+1];
+
+        return backtrack(nums, flag, n-1,  subSetSum);
+    }
+
+    private boolean backtrack(int[] nums, Boolean[][] flag, int idx, int subSetSum){
+        if(subSetSum == 0) return true;
+        if(subSetSum<0||idx == 0)
+            return false;
+
+        if(flag[idx][subSetSum]!=null)
+            return flag[idx][subSetSum];
+        boolean res =  backtrack(nums, flag, idx-1, subSetSum-nums[idx-1]) || backtrack(nums, flag, idx-1, subSetSum);
+        flag[idx][subSetSum] = res;
+        return flag[idx][subSetSum];
     }
 }
