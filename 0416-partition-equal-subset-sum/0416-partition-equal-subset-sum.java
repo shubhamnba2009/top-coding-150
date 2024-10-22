405f61ec6be29fccb1415c88dc9bedfc02cd5e47
Shubham Rajput <shubhamnba2009@gmail.com>
2024-10-22 15:15:49 +0100

    Time: 27 ms (78.53%), Space: 41.9 MB (92.38%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,26 +1,18 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int totSum = 0;
-
-        for(int n : nums) totSum +=n;
+        for(int n : nums) totSum += n;
         if(totSum%2!=0) return false;
-
         int subSetSum = totSum / 2;
-        int n = nums.length;
-        Boolean[][] flag = new Boolean[n][subSetSum+1];
 
-        return backtrack(nums, flag, n-1,  subSetSum);
-    }
+        boolean[] dp  = new boolean[subSetSum+1];
+        dp[0] = true;
+        for(int n : nums){
+            for(int i = subSetSum; i>=n;i--){
+                dp[i] = dp[i] || dp[i-n];
+            }
+        }
 
-    private boolean backtrack(int[] nums, Boolean[][] flag, int idx, int subSetSum){
-        if(subSetSum == 0) return true;
-        if(subSetSum<0||idx == 0)
-            return false;
-
-        if(flag[idx][subSetSum]!=null)
-            return flag[idx][subSetSum];
-        boolean res =  backtrack(nums, flag, idx-1, subSetSum-nums[idx-1]) || backtrack(nums, flag, idx-1, subSetSum);
-        flag[idx][subSetSum] = res;
-        return flag[idx][subSetSum];
+    return dp[subSetSum];
     }
 }
