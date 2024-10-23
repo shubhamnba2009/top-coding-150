9814f2100caa7f25273bba391707f5c90e938422
Shubham Rajput <shubhamnba2009@gmail.com>
2024-10-23 14:46:33 +0100

    Time: 1 ms (99.29%), Space: 44.5 MB (78.16%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] map = new int[26];
-
-        for(char a:magazine.toCharArray()) map[a-'a']++;
-
+        int[] mag = new int[26];
+        for(char a:magazine.toCharArray()) mag[a-'a']++;
         for(char a:ransomNote.toCharArray()){
-            if(map[a-'a']==0) return false;
-            map[a-'a']--;
+            --mag[a-'a'];
+            if(mag[a-'a']<0) return false;
         }
         return true;
     }
