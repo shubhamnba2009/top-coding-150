f31674d39636a9a43216e5cef0e792ff2bc33109
Shubham Rajput <shubhamnba2009@gmail.com>
2024-10-24 13:30:27 +0100

    Time: 16 ms (75.09%), Space: 44.1 MB (65.96%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,36 +1,31 @@
 class Solution {
-    public String longestPalindrome(String s){ 
-        if (s == null || s.length() < 1) {
-            return "";
-        }
+    public String longestPalindrome(String s) {
+        if(s==null ||s.length()<1) return "";
 
-        int start = 0, end = 0;
-        
-        for (int i = 0; i < s.length(); i++) {
-            // Odd length palindromes (centered at a single character)
-            int len1 = expandAroundCenter(s, i, i);
-            // Even length palindromes (centered between two characters)
-            int len2 = expandAroundCenter(s, i, i + 1);
-            
-            int len = Math.max(len1, len2);
-            
-            if (len > end - start) {
-                // Update the start and end index of the longest palindrome found
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+        int start = 0, end = 0, maxLen = 0;
+
+        for(int i = 0;i<s.length();i++){
+            int[] f = expand(s, i, i);
+            int[] f1 = expand(s, i, i+1);
+
+            if(f[1]-f[0]>f1[1]-f1[0]){
+                if(f[1]-f[0]>end-start){
+                    start = f[0];end = f[1];
+                }
+            } else {
+                if(f1[1]-f1[0]>end-start){
+                start = f1[0]; end = f1[1];
+                }
             }
         }
-        
-        return s.substring(start, end + 1);
+
+        return s.substring(start, end+1);
     }
 
-    private int expandAroundCenter(String s, int left, int right) {
-        // Expand around the center as long as the characters are equal
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
+    private int[] expand(String s, int left, int right){
+        while(left>=0&&right<s.length()&&s.charAt(left) == s.charAt(right)){
+            left--;right++;
         }
-        // Return the length of the palindrome
-        return right - left - 1;
+        return new int[]{left+1, right-1};
     }
 }
