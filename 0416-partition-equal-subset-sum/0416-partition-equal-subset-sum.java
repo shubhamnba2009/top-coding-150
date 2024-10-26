fbf1eac39657e0cc8af6659fbb50102ee9538dd5
Shubham Rajput <shubhamnba2009@gmail.com>
2024-10-26 17:57:43 +0100

    Time: 25 ms (84.13%), Space: 42.1 MB (87.99%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -3,16 +3,16 @@ class Solution {
         int totSum = 0;
         for(int n : nums) totSum += n;
         if(totSum%2!=0) return false;
-        int subSetSum = totSum / 2;
+        int subSetSum = totSum/2;
 
-        boolean[] dp  = new boolean[subSetSum+1];
+        boolean[] dp = new boolean[subSetSum+1];
         dp[0] = true;
         for(int n : nums){
-            for(int i = subSetSum; i>=n;i--){
+            for(int i = subSetSum;i>=n;i--) {
                 dp[i] = dp[i] || dp[i-n];
             }
         }
 
-    return dp[subSetSum];
+        return dp[subSetSum];
     }
 }
