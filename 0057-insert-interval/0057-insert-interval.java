7175cf36d340087432b2eb0a018ece7e01f27571
Shubham Rajput <shubhamnba2009@gmail.com>
2025-01-23 21:15:18 +0000

    Time: 1 ms (98.96%), Space: 45.1 MB (26.39%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,21 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> res = new ArrayList<>();
-        int i = 0, len = intervals.length;
+        List<int[]> result = new ArrayList<>();
+        int i = 0;
+        int n = intervals.length;
 
-        while(i<len&&intervals[i][1]<newInterval[0])
-            res.add(intervals[i++]);
-
-        while(i<len&&intervals[i][0]<=newInterval[1]){
-            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
-            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+        // Add all intervals before the newInterval that do not overlap
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            result.add(intervals[i]);
             i++;
         }
-        res.add(newInterval);
 
-        while(i<len)
-            res.add(intervals[i++]);
+        // Merge overlapping intervals
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        result.add(newInterval);
 
-        return res.toArray(new int[res.size()][]);
+        // Add remaining intervals
+        while (i < n) {
+            result.add(intervals[i]);
+            i++;
+        }
+
+        // Convert List<int[]> to int[][]
+        return result.toArray(new int[result.size()][]);
     }
 }
