75b4b9971aa95c88ba8a195c6dd5f7718e2d61e8
Shubham Rajput <shubhamnba2009@gmail.com>
2026-02-02 23:52:34 +0000

    Time: 4 ms (79.95%), Space: 44.9 MB (54.78%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,11 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        int[] map = new int[256];
-        for(char c:s.toCharArray()) map[c]++;
-        for(char c:t.toCharArray()){
-            if(map[c]==0)   return false;
-            map[c]--;
+        int[] map = new int[26];
+
+        for(char ch: s.toCharArray())map[ch-'a']++;
+        for(char ch: t.toCharArray()){
+            if(map[ch-'a']==0) return false;
+            map[ch-'a']--;
+        }
+        for(int i =0;i<26;i++)
+        {
+            if(map[i]!=0) return false;
         }
         return true;
     }
