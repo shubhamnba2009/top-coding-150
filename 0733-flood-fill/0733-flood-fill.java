c516ca4cf85f58261dee764c238d10b5ce4e772b
Shubham Rajput <shubhamnba2009@gmail.com>
2026-02-03 22:00:16 +0000

    Time: 0 ms (100%), Space: 46.9 MB (38.94%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,17 +1,16 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        dfs(image, sr, sc, image[sr][sc], color);
+        dfs(image, sr, sc, color, image[sr][sc]);
         return image;
     }
 
-    public void dfs(int[][] image, int sr, int sc, int oc, int color){
-        if(sr<0||sc<0||sr>image.length-1||sc>image[0].length-1||image[sr][sc]!=oc||image[sr][sc]==color)
+    private void dfs(int[][] image, int sr, int sc, int nc, int oo) {
+        if(sc>=image[0].length||sr>=image.length||sr<0||sc<0||image[sr][sc]==nc||image[sr][sc]!=oo)
             return;
-
-        image[sr][sc] = color;
-        dfs(image, sr+1, sc, oc, color);
-        dfs(image, sr, sc+1, oc, color);
-        dfs(image, sr, sc-1, oc, color);
-        dfs(image, sr-1, sc, oc, color);
+        image[sr][sc] = nc;
+        dfs(image, sr+1, sc, nc, oo);
+        dfs(image, sr-1, sc, nc, oo);
+        dfs(image, sr, sc+1, nc, oo);
+        dfs(image, sr, sc-1, nc, oo);
     }
 }
