382c2cf5776ca711b640d0504ad94d913fadbb23
Shubham Rajput <shubhamnba2009@gmail.com>
2026-04-15 22:30:12 +0100

    Time: 27 ms (88.71%), Space: 48.3 MB (56.19%) - LeetHub

--- 1797-design-authentication-manager/1797-design-authentication-manager.java
+++ 1797-design-authentication-manager/1797-design-authentication-manager.java
@@ -1,74 +1,84 @@
 class AuthenticationManager {
-
-    private int timeToLive;
-    private final HashMap<String, Node> tokenToTtl;
-    private Node dummyHead;
-    private Node dummyTail;
-
+    class Store{
+        String key;
+        int time;
+        public Store(String key, int time){
+            this.key = key;
+            this.time = time;
+        }
+    }
+    ArrayList<Store> order;
+    int limit;
+    HashMap<String, Integer> map;
     public AuthenticationManager(int timeToLive) {
-        this.timeToLive = timeToLive;
-        tokenToTtl = new HashMap<>();
-        dummyHead = new Node("InvalidHeaderToken", -1);
-        dummyTail = new Node("InvalidTailToken", -1);
-        dummyHead.next = dummyTail;
-        dummyTail.prev = dummyHead;
-    }
-    
-    public void generate(String tokenId, int currentTime) {
-        Node node = new Node(tokenId, currentTime + timeToLive);
-        tokenToTtl.put(tokenId, node);
-        add(node);
+        order = new ArrayList<>();
+        map = new HashMap<>();
+        limit = timeToLive;
     }
 
-    private void add(Node node){
-        Node next = dummyHead.next;
-        dummyHead.next = node;
-        node.next = next;
-        node.prev = dummyHead;
-        next.prev = node;
-    }
-    
-    private void remove(Node node){
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
+    public void generate(String tokenId, int currentTime) {
+        map.put(tokenId, currentTime+limit);
+        int ind = binaryInd(currentTime+limit);
+        order.add(ind, new Store(tokenId, currentTime+limit));
     }
 
     public void renew(String tokenId, int currentTime) {
-        Node node = tokenToTtl.get(tokenId);
-        if (node == null) return;
-        remove(node);
-        if (node.expiryTime <= currentTime){
-            tokenToTtl.remove(tokenId); //remove as expired
-            return; //don't renew as expired
-        }
-        node = new Node(tokenId, currentTime + timeToLive);
-        tokenToTtl.put(tokenId, node);
-        add(node);
+        if(!map.containsKey(tokenId) || map.get(tokenId) <= currentTime) return;
+        int ind = binaryFnd(tokenId, map.get(tokenId));
+        order.remove(ind);
+        ind = binaryInd(currentTime+limit);
+        order.add(ind, new Store(tokenId, currentTime+limit));
+        map.put(tokenId, currentTime+limit);
     }
-    
+
     public int countUnexpiredTokens(int currentTime) {
-        Node curr = dummyTail.prev;
-        while (curr != dummyHead && curr.expiryTime <= currentTime){
-            tokenToTtl.remove(curr.tokenId); //remove as expired
-            remove(curr);
-            curr = curr.prev;
-        }
-        return tokenToTtl.size();
+        int ind = binaryInd(currentTime);
+        return order.size()-ind;
     }
-}
 
-class Node{
-    String tokenId;
-    int expiryTime;
-    Node prev;
-    Node next;
+    public int binaryInd(int tar){
+        int st = 0;
+        int ed = order.size()-1;
+        while(st <= ed){
+            int mid = st + (ed-st)/2;
+            if(order.get(mid).time <= tar){
+                st = mid+1;
+            }
+            else ed = mid-1;
+        }
 
+        return st;
+    }
 
-    Node(String tokenId, int expiryTime){
-        this.tokenId = tokenId;
-        this.expiryTime = expiryTime;
-        this.prev = null;
-        this.next = null;
+    public int binaryFnd(String tknId, int tar){
+        int st = 0;
+        int ed = order.size()-1;
+        while(st <= ed){
+            int mid = st + (ed-st)/2;
+            if(order.get(mid).time == tar){
+                st = mid;
+                break;
+            }
+            else if(order.get(mid).time < tar) st = mid+1;
+            else ed = mid-1;
+        }
+
+        int ind = st;
+        while(ind >= 0 && order.get(ind).time == tar){
+            if(order.get(ind).key.equals(tknId)){
+                return ind;
+            }
+            ind--;
+        }
+        ind = st;
+        while(ind < order.size() && order.get(ind).time == tar){
+            if(order.get(ind).key.equals(tknId)){
+                return ind;
+            }
+            ind++;
+        }
+
+        return ind;
     }
 }
 
