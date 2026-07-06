738f34b8b94c5ccab6c829d3792057a4c4bc39e7
Shubham Rajput <shubhamnba2009@gmail.com>
2026-07-06 22:54:52 +0100

    Time: 6 ms (88%), Space: 45.2 MB (79.18%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,36 +1,25 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> st = new Stack<>();
-        Function<Stack<Integer>, Integer> add = stck->{
-            int nums1 = stck.pop(); int nums2= stck.pop();
-            return nums2+nums1;
-        };
-        Function<Stack<Integer>, Integer> minus = stck->{
-            int nums1 = stck.pop(); int nums2= stck.pop();
-            return nums2-nums1;
-        };
-        Function<Stack<Integer>, Integer> div = stck->{
-            int nums1 = stck.pop(); int nums2= stck.pop();
-            return nums2/nums1;
-        };
-        Function<Stack<Integer>, Integer> mul = stck->{
-            int nums1 = stck.pop(); int nums2= stck.pop();
-            return nums2*nums1;
-        };
-        for(String str : tokens) {
-            if(str.equals("+")){
-                st.add(add.apply(st));
-            } else if (str.equals("-")){
-                st.add(minus.apply(st));
-            } else if (str.equals("*")){
-                st.add(mul.apply(st));
-            } else if (str.equals("/")){
-                st.add(div.apply(st));
+        Stack<Integer> helper = new Stack<>();
+
+        for(int i =0;i<tokens.length; i++){
+            if(tokens[i].equals("+")){
+                int sn = helper.pop(), fn = helper.pop();
+                helper.add(fn+sn);
+            } else if (tokens[i].equals("-")){
+                int sn = helper.pop(), fn = helper.pop();
+                helper.add(fn-sn);
+            } else if(tokens[i].equals("*")){
+                int sn = helper.pop(), fn = helper.pop();
+                helper.add(fn*sn);
+            } else if(tokens[i].equals("/")){
+                int sn = helper.pop(), fn = helper.pop();
+                helper.add(fn/sn);
             } else {
-                st.add(Integer.valueOf(str));
+                helper.add(Integer.parseInt(tokens[i]));
             }
         }
-        return st.pop();
 
+        return helper.pop();
     }
 }
